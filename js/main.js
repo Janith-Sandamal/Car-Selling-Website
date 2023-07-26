7112abcae3b5f42b44b74b728278606ef7e1fabd
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2023-07-26 16:54:09 +0530

    home image add and setup styles and media query

--- js/main.js
+++ js/main.js
@@ -36,3 +36,16 @@ window.onload = () =>{
 }
 
 });
+
+// js function for home page parallax effect
+
+document.querySelector('.home').addEventListener('mousemove', () => {
+    document.querySelectorAll('.home-parallax').forEach(elm => {
+        let speed = elm.getAttribute('data-speed');
+
+        let x = (window.innerWidth - e.pageX*speed)/90;
+        let y = (window.innerHeight - e.pageY*speed)/90;
+
+        elm.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+});
